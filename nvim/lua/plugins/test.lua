023384da82441ede7d0e7e42a891623b5ae02f68
starpact <yhj.await@gmail.com>
2022-01-17 00:28:54 +0800

    rust test

--- nvim/lua/plugins/test.lua
+++ nvim/lua/plugins/test.lua
@@ -7,6 +7,9 @@ vim.g["test#strategy"] = "neovim"
 vim.g["test#go#runner"] = "gotest"
 vim.g["test#go#gotest#options"] = "-v -race -failfast --count=1"
 
+vim.g["test#rust#runner"] = "cargotest"
+vim.g["test#rust#cargotest#options"] = "-- --nocapture"
+
 map("<leader>tn", "<cmd>TestNearest<CR>")
 map("<leader>tf", "<cmd>TestFile<CR>")
 map("<leader>tl", "<cmd>TestLast<CR>")
