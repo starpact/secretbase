0bcdaae26cae3d55f302cffded394041b124d00c
starpact <yhj.await@gmail.com>
2022-03-22 20:55:42 +0800

    _

---
+++ nvim/lua/lsp/settings/gopls.lua
@@ -0,0 +1,24 @@
+function GoOrganizeImports(wait_ms)
+  local params = vim.lsp.util.make_range_params()
+  params.context = { only = { "source.organizeImports" } }
+  local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
+  for _, res in pairs(result or {}) do
+    for _, r in pairs(res.result or {}) do
+      if r.edit then
+        vim.lsp.util.apply_workspace_edit(r.edit)
+      else
+        vim.lsp.buf.execute_command(r.command)
+      end
+    end
+  end
+end
+
+vim.cmd([[
+augroup GO_LSP
+  autocmd!
+  autocmd BufWritePre *.go :silent! lua GoOrganizeImports(3000)
+  autocmd BufWritePre *.go :silent! lua vim.lsp.buf.formatting_sync(nil, 1000)
+augroup END
+]])
+
+return {}
--- nvim/lua/plugins/formatter.lua
+++ nvim/lua/plugins/formatter.lua
@@ -3,21 +3,8 @@ if not ok then
   return
 end
 
-local function filepath()
-  return vim.fn.fnameescape(vim.api.nvim_buf_get_name(0))
-end
-
 formatter.setup({
   filetype = {
-    go = {
-      function()
-        return {
-          exe = "goimports",
-          args = { "-w", filepath() },
-          stdin = false,
-        }
-      end
-    },
     python = {
       function()
         return {
@@ -33,6 +20,6 @@ formatter.setup({
 vim.api.nvim_exec([[
 augroup AutoFormatGroup
   autocmd!
-  autocmd BufWritePost *.go,*.py FormatWrite
+  autocmd BufWritePost *.py FormatWrite
 augroup END
 ]], true)
