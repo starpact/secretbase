d5b83bac70ae1724f487e4eb9998a84ea2b9ae5b
starpact <yhj.await@gmail.com>
2022-03-28 17:01:10 +0800

    _

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -1,10 +1,15 @@
+-- Set tabsize.
 vim.cmd([[
 autocmd BufRead,BufNewFile *.json,*.yml,*.yaml,*.toml,*.lua,*html,*css,*.js,*.jsx,*.ts,*.tsx setlocal ts=2 sw=2
 autocmd BufRead,BufNewFile *.go setlocal expandtab!
+]])
+
+-- NvimTree auto close.
+vim.cmd([[
 autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
 ]])
 
--- For WSL.
+-- WSL.
 vim.cmd([[
 if has('wsl')
   augroup Yank
