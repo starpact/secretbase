8efc1bdf417182a50726f89ab415f76112bba3d1
starpact <yhj.await@gmail.com>
2022-04-14 02:44:43 +0800

    _

--- nvim/lua/plugins/cmp.lua
+++ nvim/lua/plugins/cmp.lua
@@ -50,6 +50,8 @@ cmp.setup({
     ["<C-b>"] = cmp.mapping.scroll_docs(-4),
     ["<C-f>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-e>"] = cmp.mapping({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
@@ -87,7 +89,6 @@ cmp.setup({
     { name = "path" },
   },
   confirm_opts = { behavior = cmp.ConfirmBehavior.Replace },
-  documentation = true,
   sorting = {
     comparators = {
       compare.score,
