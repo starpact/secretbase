e6a60421f74a0031eff11acd59c6afa3ddefde08
starpact <yhj.await@gmail.com>
2022-04-23 22:24:58 +0800

    _

--- nvim/lua/autocmd.lua
+++ nvim/lua/autocmd.lua
@@ -20,11 +20,6 @@ vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
   command = "setlocal expandtab!",
 })
 
--- NvimTree auto close.
-vim.cmd([[
-autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif
-]])
-
 -- WSL.
 vim.cmd([[
 if has('wsl')
