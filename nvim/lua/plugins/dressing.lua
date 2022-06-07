a8d6233c7ef6b5de4b92d9a313c170517d171774
starpact <yhj.await@gmail.com>
2022-06-08 01:00:50 +0800

    _

--- nvim/lua/plugins/dressing.lua
+++ nvim/lua/plugins/dressing.lua
@@ -1,7 +1,7 @@
 local dressing = require("dressing")
-
-local themes = require("telescope.themes")
-local telescope = themes.get_cursor({ initial_mode = "normal" })
+local telescope = require("telescope.themes").get_cursor({
+  initial_mode = "normal",
+})
 
 dressing.setup({
   select = {
