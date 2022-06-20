44770b02a70dd846fd39100eea5b3835d8eff6a0
starpact <yhj.await@gmail.com>
2022-06-21 00:29:22 +0800

    fix toggleterm

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,8 +1,8 @@
-function _G.set_terminal_keymaps()
-  local function map(keys, cmd)
-    vim.keymap.set("t", keys, cmd, { noremap = true })
-  end
+local function map(keys, cmd)
+  vim.keymap.set("t", keys, cmd, { noremap = true })
+end
 
+function _G.set_terminal_keymaps()
   map("<Esc>", "<C-\\><C-n>")
   map("jk", "<C-\\><C-n>")
   map("<C-h>", "<C-\\><C-n><C-W>h")
@@ -11,9 +11,22 @@ function _G.set_terminal_keymaps()
 end
 
 require("toggleterm").setup({
-  size = vim.o.lines * 0.4,
+  size = vim.o.lines * 0.3,
   open_mapping = "<C-t>",
   shade_terminals = false,
 })
 
-vim.cmd("autocmd! TermOpen term://*toggleterm#* lua set_terminal_keymaps()")
+local toggleterm_pattern = "term://*#toggleterm#*"
+
+vim.api.nvim_create_autocmd("WinEnter", {
+  pattern = toggleterm_pattern,
+  callback = function()
+    local terms = require("toggleterm.terminal")
+    local _, term = terms.identify()
+    if term then
+      term:set_mode(terms.mode.INSERT)
+    end
+  end
+})
+
+vim.cmd(string.format("autocmd! TermOpen %s lua set_terminal_keymaps()", toggleterm_pattern))
