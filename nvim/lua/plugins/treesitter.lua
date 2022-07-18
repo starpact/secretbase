3f2888ce26bd61b0f294ff40d8020659ac1ca6fa
starpact <yhj.await@gmail.com>
2022-07-18 12:30:31 +0800

    _

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,5 @@
 require("nvim-treesitter.configs").setup({
+  ensure_install = { "comment" },
   auto_install = true,
   sync_install = false,
   highlight = {
---
+++ scripts/arch_fonts.sh
@@ -0,0 +1,5 @@
+paru -Sy \
+    noto-fonts-cjk noto-fonts-emoji \
+    wqy-bitmapfont wqy-microhei wqy-microhei-lite wqy-zenhei \
+    adobe-source-code-pro-fonts adobe-source-han-sans-cn-fonts adobe-source-han-serif-cn-fonts \
+    nerd-fonts-jetbrains-mono nerd-fonts-victor-mono ttf-iosevka-nerd
