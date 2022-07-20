e6dd0405554160bf36fb521583a8c8b4c3d44d44
starpact <yhj.await@gmail.com>
2022-07-20 16:18:00 +0800

    _

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -5,12 +5,16 @@ vim.g.gruvbox_material_visual = "reverse"
 
 vim.cmd("colorscheme gruvbox-material")
 
-vim.api.nvim_set_hl(0, "VirtualTextError", { ctermfg = 167, fg = "#c14a4a" })
-vim.api.nvim_set_hl(0, "VirtualTextWarning", { ctermfg = 214, fg = "#b47109" })
-vim.api.nvim_set_hl(0, "VirtualTextInfo", { ctermfg = 109, fg = "#45707a" })
-vim.api.nvim_set_hl(0, "VirtualTextHint", { ctermfg = 142, fg = "#6c782e" })
-vim.api.nvim_set_hl(0, "TSField", { link = "Fg" })
-vim.api.nvim_set_hl(0, "TSNamespace", { link = "Fg" })
-vim.api.nvim_set_hl(0, "NvimTreeFolderIcon", { link = "Blue" })
-vim.api.nvim_set_hl(0, "FidgetTask", { link = "Comment" })
-vim.api.nvim_set_hl(0, "Search", { ctermfg = 235, ctermbg = 167, fg = "#282828", bg = "#ea6962" })
+local function hl(group, opts)
+  vim.api.nvim_set_hl(0, group, opts)
+end
+
+hl("VirtualTextError", { ctermfg = 167, fg = "#c14a4a" })
+hl("VirtualTextWarning", { ctermfg = 214, fg = "#b47109" })
+hl("VirtualTextInfo", { ctermfg = 109, fg = "#45707a" })
+hl("VirtualTextHint", { ctermfg = 142, fg = "#6c782e" })
+hl("TSField", { link = "Fg" })
+hl("TSNamespace", { link = "Fg" })
+hl("NvimTreeFolderIcon", { link = "Blue" })
+hl("FidgetTask", { link = "Comment" })
+hl("Search", { ctermfg = 235, ctermbg = 167, fg = "#282828", bg = "#ea6962" })
