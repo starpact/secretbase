513680ecb78c3d06bfdcee9cdb1224a0c02cc49f
starpact <yhj.await@gmail.com>
2022-08-27 13:58:29 -0400

    _

--- nvim/lua/plugins/snippets.lua
+++ nvim/lua/plugins/snippets.lua
@@ -2,12 +2,23 @@ local ls = require("luasnip")
 local s = ls.snippet
 local i = ls.insert_node
 local c = ls.choice_node
+local t = ls.text_node
 local fmt = require("luasnip.extras.fmt").fmt
 
 ls.add_snippets("go", {
   s("ife", fmt("if err != nil {{\n\treturn {}{}\n}}\n\n", {
     c(1, { i(nil, "nil, "), i(nil, "") }),
-    c(2, { i(nil, "err"), i(nil, "errors.Trace(err)") })
+    i(2, "err")
   })),
   s("fp", fmt("fmt.Println({})", { i(1, "") })),
+  s("todo", t([[panic("unimplemented")]]))
+})
+
+vim.api.nvim_create_autocmd("InsertLeave", {
+  callback = function()
+    if ls.session.current_nodes[vim.api.nvim_get_current_buf()]
+        and not ls.session.jump_active then
+      ls.unlink_current()
+    end
+  end,
 })
