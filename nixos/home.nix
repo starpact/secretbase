8d1970218dedc442208323e3080a251087ccdfd4
starpact <yhj.await@gmail.com>
2022-09-09 21:30:58 +0800

    _

--- nixos/home.nix
+++ nixos/home.nix
@@ -106,6 +106,23 @@
 
   programs = {
     alacritty.enable = true;
+    bash = {
+      enable = true;
+      sessionVariables = {
+        EDITOR = "nvim";
+      };
+      shellAliases = {
+        vim = "nvim";
+        ls = "exa";
+        l = "ls -al";
+        cat = "bat";
+        lg = "lazygit";
+      };
+      initExtra = ''
+        export PATH=$HOME./cargo/bin:$HOME/go/bin:$PATH
+        stty -ixon
+      '';
+    };
     wezterm.enable = true;
   };
 
