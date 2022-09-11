9066b0549ea848122e0dc5ec7508972b2099e9f7
starpact <yhj.await@gmail.com>
2022-09-11 06:13:07 -0400

    _

---
+++ dconf.nix
@@ -0,0 +1,76 @@
+{ pkgs, lib, ... }:
+
+{
+  dconf.settings = {
+    "org/gnome/mutter" = {
+      dynamic-workspaces = false;
+    };
+    "org/gnome/desktop/peripherals/touchpad" = {
+      tap-to-click = true;
+    };
+    "org/gnome/desktop/peripherals/keyboard" = {
+      delay = lib.hm.gvariant.mkUint32 300;
+      repeat-interval = lib.hm.gvariant.mkUint32 30;
+    };
+    "org/gnome/shell/keybindings" = {
+      toggle-message-tray = [ ];
+    };
+    "org/gnome/shell/keybindings" = {
+      switch-to-application-1 = [ ];
+      switch-to-application-2 = [ ];
+      switch-to-application-3 = [ ];
+      switch-to-application-4 = [ ];
+    };
+    "org/gnome/desktop/wm/keybindings" = {
+      close = [ "<Super>q" ];
+      toggle-maximized = [ "<Super>F" ];
+      switch-to-workspace-1 = [ "<Super>1" ];
+      switch-to-workspace-2 = [ "<Super>2" ];
+      switch-to-workspace-3 = [ "<Super>3" ];
+      switch-to-workspace-4 = [ "<Super>4" ];
+      switch-to-workspace-left = [ "<Super><Control>H" ];
+      switch-to-workspace-right = [ "<Super><Control>L" ];
+      move-to-workspace-1 = [ "<Super><Shift>1" ];
+      move-to-workspace-2 = [ "<Super><Shift>2" ];
+      move-to-workspace-3 = [ "<Super><Shift>3" ];
+      move-to-workspace-4 = [ "<Super><Shift>4" ];
+      move-to-workspace-left = [ "<Super><Shift>H" ];
+      move-to-workspace-right = [ "<Super><Shift>L" ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      home = [ "<Super>E" ];
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3/"
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4/"
+      ];
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      binding = "<Super>Return";
+      command = "alacritty";
+      name = "Terminal";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom1" = {
+      binding = "<Super>E";
+      command = "nautilus";
+      name = "Files";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom2" = {
+      binding = "<Super>B";
+      command = "google-chrome-stable";
+      name = "Browser";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom3" = {
+      binding = "F1";
+      command = "flameshot gui";
+      name = "Flameshot";
+    };
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom4" = {
+      binding = "<Super>I";
+      command = "alacritty -e htop";
+      name = "Htop";
+    };
+  };
+}
