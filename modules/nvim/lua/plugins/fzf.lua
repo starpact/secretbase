0f4d2eaa758553325394d50dbbde88525bc1eee6
starpact <yhj.await@gmail.com>
2022-12-21 23:02:39 +0800

    _

--- modules/nvim/lua/plugins/fzf.lua
+++ modules/nvim/lua/plugins/fzf.lua
@@ -1,53 +1,50 @@
 local fzf = require("fzf-lua")
 
-local function with_opt(opt)
-  return function(command)
-    return function()
-      command(opt)
-    end
-  end
-end
-
-local opt_no_preview = {
-  winopts = {
-    hl = { cursorline = "" },
-    preview = { hidden = "hidden" },
-  },
+local default_winopts = {
+  height = 0.3,
+  width = 1,
+  row = 1,
+  col = 0,
+  hl = { cursorline = "visual" },
+  border = { "─", "─", "─", "", "", "", "", "" },
+  preview = { delay = 0, horizontal = "right:50%" },
 }
-local opt_fullscreen = { winopts = { fullscreen = true } }
+local no_preview_winopts = {
+  hl = { cursorline = "" },
+  preview = { hidden = "hidden" },
+}
+local git_opt = {
+  winopts = {
+    fullscreen = true,
+    preview = { horizontal = "up:70%" },
+  },
+  preview_pager = "delta -s --width=$FZF_PREVIEW_COLUMNS",
+}
 
-vim.keymap.set("n", "<leader>f", with_opt(opt_no_preview)(fzf.files))
-vim.keymap.set("n", "<leader>o", with_opt(opt_no_preview)(fzf.oldfiles))
-vim.keymap.set("n", "<leader>b", with_opt(opt_no_preview)(fzf.buffers))
+vim.keymap.set("n", "<leader>f", fzf.files)
+vim.keymap.set("n", "<leader>o", fzf.oldfiles)
+vim.keymap.set("n", "<leader>b", fzf.buffers)
 vim.keymap.set("n", "<leader>/", fzf.live_grep)
 vim.keymap.set("n", "<leader>?", fzf.grep_cword)
 vim.keymap.set("n", "<leader>d", fzf.diagnostics_workspace)
-vim.keymap.set("n", "<leader>gs", with_opt(opt_fullscreen)(fzf.git_status))
-vim.keymap.set("n", "<leader>gc", with_opt(opt_fullscreen)(fzf.git_bcommits))
+vim.keymap.set("n", "<leader>gs", fzf.git_status)
+vim.keymap.set("n", "<leader>gc", fzf.git_bcommits)
 vim.keymap.set("n", "<leader>l", function()
-  fzf.builtin({ winopts = { height = 0.3, width = 1, row = 1, col = 0 } })
+  fzf.builtin({ winopts = default_winopts })
 end)
 
+fzf.register_ui_select()
 fzf.setup({
-  winopts = {
-    height = 0.3,
-    width = 1,
-    row = 1,
-    col = 0,
-    hl = { cursorline = "visual" },
-    border = { "─", "─", "─", "", "", "", "", "" },
-    preview = { delay = 0, horizontal = "right:50%" },
-  },
+  winopts = default_winopts,
   fzf_opts = {
     ["--history"] = vim.fn.stdpath("data") .. "/fzf-lua-history",
     ["--no-separator"] = "",
   },
-  files = { git_icons = false },
-  git = {
-    status = { preview_pager = "delta" },
-    commits = { preview_pager = "delta" },
-    bcommits = { preview_pager = "delta" },
-  },
+  files = { winopts = no_preview_winopts, git_icons = false },
+  oldfiles = { winopts = no_preview_winopts },
+  buffers = { winopts = no_preview_winopts },
+  grep = { git_icons = false },
+  git = { status = git_opt, commits = git_opt, bcommits = git_opt },
   keymap = {
     fzf = {
       ["ctrl-n"] = "down",
