61033fde7bfbddc492a4b803f19fcb00795f668e
starpact <yhj.await@gmail.com>
2023-01-03 18:54:19 +0800

    _

--- modules/nvim/lua/plugins/colorscheme.lua
+++ modules/nvim/lua/plugins/colorscheme.lua
@@ -1,20 +1,22 @@
 vim.g.everforest_background = "hard"
 vim.g.everforest_disable_italic_comment = 1
 vim.g.everforest_transparent_background = 1
-
 vim.cmd.colorscheme("everforest")
-local function hl(group, opts)
-  vim.api.nvim_set_hl(0, group, opts)
-end
 
-hl("VirtualTextError", { ctermfg = 167, fg = "#f85552" })
-hl("VirtualTextWarning", { ctermfg = 214, fg = "#dfa000" })
-hl("VirtualTextInfo", { ctermfg = 109, fg = "#3a94c5" })
-hl("VirtualTextHint", { ctermfg = 142, fg = "#8da101" })
+vim.api.nvim_set_hl(0, "VirtualTextError", { ctermfg = 167, fg = "#f85552" })
+vim.api.nvim_set_hl(0, "VirtualTextWarning", { ctermfg = 214, fg = "#dfa000" })
+vim.api.nvim_set_hl(0, "VirtualTextInfo", { ctermfg = 109, fg = "#3a94c5" })
+vim.api.nvim_set_hl(0, "VirtualTextHint", { ctermfg = 142, fg = "#8da101" })
 
-hl("@field", { link = "Fg" })
-hl("@namespace", { link = "Fg" })
-hl("@property", { link = "Fg" })
-hl("@type.qualifier", { link = "Red" })
-hl("@storageclass", { link = "Red" })
-hl("@type.definition", { link = "Yellow" })
+vim.api.nvim_set_hl(0, "@constant.builtin", { link = "Blue" })
+vim.api.nvim_set_hl(0, "@field", { link = "Fg" })
+vim.api.nvim_set_hl(0, "@namespace", { link = "Fg" })
+vim.api.nvim_set_hl(0, "@property", { link = "Fg" })
+vim.api.nvim_set_hl(0, "@storageclass", { link = "Red" })
+vim.api.nvim_set_hl(0, "@type.definition", { link = "Yellow" })
+vim.api.nvim_set_hl(0, "@type.qualifier", { link = "Red" })
+vim.api.nvim_set_hl(0, "@variable.builtin", { link = "Blue" })
+
+vim.api.nvim_set_hl(0, "@constant.builtin.go", { link = "Blue" })
+vim.api.nvim_set_hl(0, "@include.go", { link = "Red" })
+vim.api.nvim_set_hl(0, "@property.go", { link = "Fg" })
