a528597c1634ffa756979ce34cfd7fffddddd339
starpact <yhj.await@gmail.com>
2023-01-10 23:38:06 +0800

    _

--- modules/nvim/lua/modules/misc.lua
+++ modules/nvim/lua/modules/misc.lua
@@ -9,7 +9,7 @@ require("leap").set_default_keymaps()
 
 require("harpoon").setup()
 vim.keymap.set("n", "<leader>m", require("harpoon.mark").add_file)
-vim.keymap.set("n", "<C-p>", require("harpoon.ui").toggle_quick_menu)
+vim.keymap.set("n", "<C-m>", require("harpoon.ui").toggle_quick_menu)
 vim.keymap.set("n", "gn", require("harpoon.ui").nav_next)
 vim.keymap.set("n", "gp", require("harpoon.ui").nav_prev)
 
