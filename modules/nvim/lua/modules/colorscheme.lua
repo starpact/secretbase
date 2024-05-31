b752485403b9304f50cbb678e34a538158e1a51c
starpact <yhj.await@gmail.com>
2024-05-31 21:06:07 +0800

    _

--- modules/nvim/lua/modules/colorscheme.lua
+++ modules/nvim/lua/modules/colorscheme.lua
@@ -1,24 +1,24 @@
-vim.o.background = "dark"
+vim.o.background = "light"
 
 vim.api.nvim_set_hl(0, "@property", { link = "@variable" })
 vim.api.nvim_set_hl(0, "@module", { link = "@variable" })
 vim.api.nvim_set_hl(0, "Type", { link = "Identifier" })
 vim.api.nvim_set_hl(0, "@type.builtin", { link = "Type" })
+vim.api.nvim_set_hl(0, "Comment", { fg = "#787878" })
 
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "NvimDarkGrey3" })
-vim.api.nvim_set_hl(0, "StatusLine", { fg = "NvimLightGrey1", bg = "NvimDarkGrey3" })
-vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "NvimLightGrey4", bg = "NvimDarkGrey3" })
-vim.api.nvim_set_hl(0, "StatusLineError", { fg = "NvimLightRed", bg = "NvimDarkGrey3" })
-vim.api.nvim_set_hl(0, "StatusLineWarn", { fg = "NvimLightYellow", bg = "NvimDarkGrey3" })
-vim.api.nvim_set_hl(0, "StatusLineInfo", { fg = "NvimLightCyan", bg = "NvimDarkGrey3" })
-vim.api.nvim_set_hl(0, "StatusLineHint", { fg = "NvimLightBlue", bg = "NvimDarkGrey3" })
+vim.api.nvim_set_hl(0, "StatusLine", { fg = "NvimDarkGrey1", bg = "NvimLightGrey3" })
+vim.api.nvim_set_hl(0, "StatusLineNC", { fg = "NvimDarkGrey4", bg = "NvimLightGrey3" })
+vim.api.nvim_set_hl(0, "StatusLineError", { fg = "NvimDarkRed", bg = "NvimLightGrey3" })
+vim.api.nvim_set_hl(0, "StatusLineWarn", { fg = "NvimDarkYellow", bg = "NvimLightGrey3" })
+vim.api.nvim_set_hl(0, "StatusLineInfo", { fg = "NvimDarkCyan", bg = "NvimLightGrey3" })
+vim.api.nvim_set_hl(0, "StatusLineHint", { fg = "NvimDarkBlue", bg = "NvimLightGrey3" })
 
-vim.api.nvim_set_hl(0, "FzfLuaBufFlagCur", { fg = "NvimLightRed" })
-vim.api.nvim_set_hl(0, "FzfLuaHeaderText", { fg = "NvimLightRed" })
-vim.api.nvim_set_hl(0, "FzfLuaLiveSym", { fg = "NvimLightRed" })
-vim.api.nvim_set_hl(0, "FzfLuaHeaderBind", { fg = "NvimLightYellow" })
-vim.api.nvim_set_hl(0, "FzfLuaTabMarker", { fg = "NvimLightYellow" })
-vim.api.nvim_set_hl(0, "FzfLuaBufNr", { fg = "NvimLightYellow" })
+vim.api.nvim_set_hl(0, "FzfLuaBufFlagCur", { fg = "NvimDarkRed" })
+vim.api.nvim_set_hl(0, "FzfLuaHeaderText", { fg = "NvimDarkRed" })
+vim.api.nvim_set_hl(0, "FzfLuaLiveSym", { fg = "NvimDarkRed" })
+vim.api.nvim_set_hl(0, "FzfLuaHeaderBind", { fg = "NvimDarkYellow" })
+vim.api.nvim_set_hl(0, "FzfLuaTabMarker", { fg = "NvimDarkYellow" })
+vim.api.nvim_set_hl(0, "FzfLuaBufNr", { fg = "NvimDarkYellow" })
 
 for _, group in ipairs(vim.fn.getcompletion("@lsp", "highlight")) do
   vim.api.nvim_set_hl(0, group, {})
